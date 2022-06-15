d1433ae51ec12e88b623809a6b42dfedbc306f10
Steven Lin <steven_lx@outlook.com>
2022-06-15 11:18:57 -0700

    add more comments

--- solution.cpp
+++ solution.cpp
@@ -1,19 +1,12 @@
 #include "solution.h"
 #include "util.h"
-#include <unordered_set>
-#include <numeric>
-#include <iterator>
-#include <unordered_map>
-#include <deque>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <stack>
+
 using namespace sol226;
 using namespace std;
 
 /*takeaways
   - swap left and right recursively
+  - time complexity O(n)
 */
 Node *Solution::invertTree(Node *root)
 {
